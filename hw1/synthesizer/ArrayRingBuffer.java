639f0874408706a0b9011d6ed1cc8bc25c2fea56
Kaiwen <kaiwenliu97@gmail.com>
2022-12-29 15:06:35 -0700

    finish hw1 except for TTFAF

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,10 @@
-// TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+
+package synthesizer;
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> implements Iterable<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +21,11 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[capacity];
+        first=0;
+        last=0;
+        fillCount=0;
+        this.capacity=capacity;
     }
 
     /**
@@ -30,6 +35,17 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) throw new RuntimeException("Ring buffer overflow");
+        if (fillCount!=0) {
+            if (last >= capacity - 1) {
+                last = 0;
+            } else {
+                last++;
+            }
+        }
+        fillCount++;
+        rb[last] = x;
+        return;
     }
 
     /**
@@ -38,7 +54,17 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if (isEmpty()) throw new RuntimeException("Ring buffer underflow");
+        T returnItem = rb[first];
+        rb[first] = null;
+        if (first >= capacity-1) {
+            first = 0;
+        }else{
+            first++;
+        }
+        fillCount--;
+        return returnItem;
     }
 
     /**
@@ -46,7 +72,30 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+    public class ArrayRingBufferIterator implements Iterator<T>{
+        private int sentinel;
+        public ArrayRingBufferIterator() {
+            sentinel = 0;
+        }
+        public boolean hasNext(){
+            return sentinel != fillCount;
+        }
+        public T next() {
+            T returnItem = rb[sentinel];
+            sentinel += 1;
+            return returnItem;
+        }
+    }
+
 }
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -8,8 +8,57 @@ import static org.junit.Assert.*;
 
 public class TestArrayRingBuffer {
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void iterateTest() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(3);
+        assertEquals(0, arb.fillCount());
+        assertEquals(3, arb.capacity());
+        String str1 = "abc";
+        String str2 = "gef";
+        String str3 = "jjk";
+        arb.enqueue(str1);
+        arb.enqueue(str2);
+        arb.enqueue(str3);
+        for (String i : arb) {
+            System.out.println(i);
+        }
+    }
+
+    @Test
+    public void enqueueTest() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(3);
+        String str1 = "abc";
+        String str2 = "gef";
+        String str3 = "jjk";
+        String str4 = "dfde";
+        arb.enqueue(str1);
+        arb.enqueue(str2);
+        arb.enqueue(str3);
+        try{
+            arb.enqueue(str4);
+        } catch (RuntimeException re) {
+            String reStr = "Ring buffer overflow";
+            assertEquals(reStr, re.getMessage());
+        }
+    }
+    @Test
+    public void dequeueTest() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(3);
+        String str1 = "abc";
+        String str2 = "gef";
+        String str3 = "jjk";
+        String str4 = "dfde";
+        arb.enqueue(str1);
+        arb.enqueue(str2);
+        arb.enqueue(str3);
+        arb.dequeue();
+        arb.dequeue();
+        arb.dequeue();
+        try{
+            arb.dequeue();
+        } catch (RuntimeException re) {
+            String reStr = "Ring buffer underflow";
+            assertEquals(reStr, re.getMessage());
+        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
